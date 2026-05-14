72a0cec5ecb057269ea5957a3b888120b1e210bf
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-14 15:46:27 +0530

    query generic fields
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- playground/views.py
+++ playground/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-from django.db.models import Value, F, Func, Count, ExpressionWrapper, DecimalField
-from django.db.models.functions import Concat
-from store.models import Product, Customer
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product
+from tags.models import TaggedItem
 
 # Create your views here.
 
 
 def say_hello(request):
-    discounted_price = ExpressionWrapper(
-        F("unit_price") * 0.8, output_field=DecimalField()
+    content_type = ContentType.objects.get_for_model(Product)
+    query_set = TaggedItem.objects.select_related("tag").filter(
+        content_type=content_type, object_id=1
     )
-    query_set = Product.objects.annotate(discounted_price=discounted_price)
-
-    return render(request, "hello.html", {"name": "Indra", "result": list(query_set)})
+    return render(request, "hello.html", {"name": "Indra", "tags": list(query_set)})
