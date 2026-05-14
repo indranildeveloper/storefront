2cc8704fab46b0f4cb297266e38e02213bd6a899
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-14 19:43:51 +0530

    transactions
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,26 @@
 from django.shortcuts import render
-from django.contrib.contenttypes.models import ContentType
-from store.models import Product
-from tags.models import TaggedItem
+from django.db import transaction
+from store.models import Order, OrderItem, Product, Customer, Collection
 
 # Create your views here.
 
 
 def say_hello(request):
-    query_set = TaggedItem.objects.get_tags_for(Product, 1)
-    return render(request, "hello.html", {"name": "Indra", "tags": list(query_set)})
+    with transaction.atomic():
+        order = Order()
+        order.customer_id = 1
+        order.save()
+
+        item = OrderItem()
+        item.order = order
+        item.product_id = -1
+        item.quantity = 1
+        # pyrefly: ignore [bad-assignment]
+        item.unit_price = 10
+        item.save()
+
+    return render(
+        request,
+        "hello.html",
+        {"name": "Indra"},
+    )
