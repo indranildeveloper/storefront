d6ee5f8bb90b049376f433f7da80658e9c088079
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-14 11:51:41 +0530

    retrieve data from database
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from store.models import Product
 
 # Create your views here.
 
 
 def say_hello(request):
-    x = 1
-    y = 2
+    exists = Product.objects.filter(pk=0).exists()
+
     return render(request, "hello.html", {"name": "Indra"})
