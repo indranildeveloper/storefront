53bede88416dd203f8f4c132422f3a54cb340eae
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-14 13:35:18 +0530

    selecting related objects
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 
-from store.models import Product
+from store.models import Order
 
 # Create your views here.
 
 
 def say_hello(request):
-    query_set = Product.objects.defer("description")
+    # select_related (1)
+    # prefetch_related (n)
+    # query_set = Product.objects.select_related("collection").all()
+    # query_set = (
+    #     Product.objects.prefetch_related("promotions")
+    #     .select_related("collection")
+    #     .all()
+    # )
 
-    return render(request, "hello.html", {"name": "Indra", "products": list(query_set)})
+    query_set = (
+        Order.objects.select_related("customer")
+        .prefetch_related("orderitem_set__product")
+        .order_by("-placed_at")[:5]
+    )
+
+    return render(request, "hello.html", {"name": "Indra", "orders": list(query_set)})
