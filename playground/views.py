0ad2c41f1c6fc98ea9ce8933dc1a906c24ad5c56
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-14 12:24:40 +0530

    limit objects
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- playground/views.py
+++ playground/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
 
-# from django.db.models import Q, F
-
 from store.models import Product
 
 # Create your views here.
 
 
 def say_hello(request):
-    product = Product.objects.order_by("unit_price")[0]
-    product = Product.objects.latest("unit_price")
+    query_set = Product.objects.all()[5:10]
 
-    return render(request, "hello.html", {"name": "Indra", "product": product})
+    return render(request, "hello.html", {"name": "Indra", "products": list(query_set)})
