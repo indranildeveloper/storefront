4b0339e8cdb582822c94178a4186912ec532b54c
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-14 20:32:21 +0530

    select related fields
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- store/models.py
+++ store/models.py
@@ -57,7 +57,10 @@ class Customer(models.Model):
     )
 
     def __str__(self) -> str:
-        return f"{self.first_name}"
+        return f"{self.first_name} {self.last_name}"
+
+    class Meta:
+        ordering = ["first_name", "last_name"]
 
 
 class Order(models.Model):
