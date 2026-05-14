f06420dae198c23dbd3189c041ec3c1ed91f6f5d
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-14 12:34:11 +0530

    select fields to query
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
-from store.models import Product
+from store.models import Product, OrderItem
 
 # Create your views here.
 
 
 def say_hello(request):
-    query_set = Product.objects.all()[5:10]
+    query_set = Product.objects.filter(
+        id__in=OrderItem.objects.values("product_id").distinct()
+    ).order_by("title")
 
     return render(request, "hello.html", {"name": "Indra", "products": list(query_set)})
