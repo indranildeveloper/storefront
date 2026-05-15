80910de7abbbd55cab5099e12c3f684e706906f5
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-15 11:52:32 +0530

    add serializers
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- store/urls.py
+++ store/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("products/", views.product_list, name="list-products"),
-    path("products/<int:id>/", views.product_detail, name="detail-product"),
+    path("products/<int:product_id>/", views.product_detail, name="detail-product"),
 ]
