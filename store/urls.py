a79cb2655441f995228a63ac364710358da5af78
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-15 12:23:02 +0530

    model relationships
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- store/urls.py
+++ store/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products/", views.product_list, name="list-products"),
-    path("products/<int:product_id>/", views.product_detail, name="detail-product"),
+    path("products/", views.product_list, name="products-list"),
+    path("products/<int:product_id>/", views.product_detail, name="product-detail"),
+    path(
+        "collection/<int:pk>/",
+        views.collection_detail,
+        name="collection-detail",
+    ),
 ]
--- store/views.py
+++ store/views.py
@@ -9,8 +9,8 @@ from .serializers import ProductSerializer
 # Create your views here.
 @api_view()
 def product_list(request):
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
+    queryset = Product.objects.select_related("collection").all()
+    serializer = ProductSerializer(queryset, many=True, context={"request": request})
     return Response(serializer.data)
 
 
@@ -19,3 +19,8 @@ def product_detail(request, product_id):
     product = get_object_or_404(Product, pk=product_id)
     serializer = ProductSerializer(product)
     return Response(serializer.data)
+
+
+@api_view()
+def collection_detail(request, pk):
+    return Response("ok")
