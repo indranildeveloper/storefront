9aed97743446c6bd790f591c16fd885e8236bc4d
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-16 18:45:39 +0530

    router in django rest framework
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- store/views.py
+++ store/views.py
@@ -1,64 +1,43 @@
 from django.db.models.aggregates import Count
-from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 
-from store.models import Collection
+from store.models import Collection, OrderItem
 
 from .models import Product
 from .serializers import CollectionSerializer, ProductSerializer
 
-# from rest_framework.views import APIView
-# from rest_framework.mixins import CreateModelMixin, ListModelMixin
-
 
 # Create your views here.
-class ProductList(ListCreateAPIView):
-    queryset = Product.objects.select_related("collection").all()
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
     def get_serializer_context(self):
         return {"request": self.request}
 
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
-
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        # pyrefly: ignore [missing-attribute]
-        if product.orderitems.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs["pk"]).count() > 0:
             return Response(
                 {
                     "error": "Product can not be deleted because it is associated with and order item."
                 },
                 status=status.HTTP_405_METHOD_NOT_ALLOWED,
             )
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
 
 
-class CollectionList(ListCreateAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count("products")).all()
     serializer_class = CollectionSerializer
 
-
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Collection.objects.annotate(products_count=Count("products"))
-    serializer_class = CollectionSerializer
-
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        # pyrefly: ignore [missing-attribute]
-        if collection.products.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs["pk"]).count() > 0:
             return Response(
                 {
                     "error": "Collection can not be deleted because it includes one or more products."
                 },
                 status=status.HTTP_405_METHOD_NOT_ALLOWED,
             )
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
