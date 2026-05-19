0bbf5e9467858a6917879d358aceede15e67bf1d
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-19 12:30:13 +0530

    add custom permissions
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- store/models.py
+++ store/models.py
@@ -95,6 +95,9 @@ class Order(models.Model):
     placed_at = models.DateTimeField(auto_now_add=True)
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
 
+    class Meta:
+        permissions = [("cancel_order", "Can cancel order")]
+
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT)
