ffc792b3b29ae87fa7a2f7d7ac42ef6b00eeab3c
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-19 17:52:03 +0530

    get user profile
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- core/serializers.py
+++ core/serializers.py
@@ -1,4 +1,5 @@
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import UserSerializer as BaseUserSerializer
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
@@ -12,3 +13,9 @@ class UserCreateSerializer(BaseUserCreateSerializer):
             "first_name",
             "last_name",
         ]
+
+
+class UserSerializer(BaseUserSerializer):
+    class Meta(BaseUserSerializer.Meta):
+        # pyrefly: ignore [bad-override-mutable-attribute]
+        fields = ["id", "username", "email", "first_name", "last_name"]
--- storefront/settings.py
+++ storefront/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 import os
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -164,12 +165,16 @@ REST_FRAMEWORK = {
     ),
 }
 
-SIMPLE_JWT = {
-    "AUTH_HEADER_TYPES": ("JWT",),
-}
-
 AUTH_USER_MODEL = "core.User"
 
 DJOSER = {
-    "SERIALIZERS": {"user_create": "core.serializers.UserCreateSerializer"},
+    "SERIALIZERS": {
+        "user_create": "core.serializers.UserCreateSerializer",
+        "current_user": "core.serializers.UserSerializer",
+    },
+}
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("JWT",),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
 }
