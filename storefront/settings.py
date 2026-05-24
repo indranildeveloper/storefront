43d36eee1fb14656c529e91163b28462d67a4621
Indranil Halder <indranilhalder.dev@gmail.com>
2026-05-24 12:54:47 +0530

    add cors allowed origins
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- storefront/settings.py
+++ storefront/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "djoser",
     "django_filters",
+    "corsheaders",
     "debug_toolbar",
     "playground",
     "store",
@@ -51,6 +52,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -64,6 +66,8 @@ MIDDLEWARE = [
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+CORS_ALLOWED_ORIGINS = ["http://localhost:8001", "http://127.0.0.1:8001"]
 
 ROOT_URLCONF = "storefront.urls"
 
