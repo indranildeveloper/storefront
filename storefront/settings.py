ef6421b0f59feaf1147f2132c9de55ca9921e4d6
Indranil Halder <indranilhalder.dev@gmail.com>
2026-06-07 13:39:35 +0530

    create and run tests
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- storefront/settings.py
+++ storefront/settings.py
@@ -201,8 +201,7 @@ CELERY_BROKER_URL = "redis://localhost:6379/1"
 CELERY_BEAT_SCHEDULE = {
     "notify_customers": {
         "task": "playground.tasks.notify_customers",
-        # "schedule": crontab(day_of_week=1, hour=7, minute=30),
-        "schedule": 5,
+        "schedule": crontab(day_of_week=1, hour=7, minute=30),
         "args": ["Hello World"],
     }
 }
