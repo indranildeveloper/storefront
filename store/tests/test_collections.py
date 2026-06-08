082aed77454b5c69ca10efc9ce514797a67994a7
Indranil Halder <indranilhalder.dev@gmail.com>
2026-06-08 12:31:52 +0530

    add test for authenticating users
    Signed-off-by: Indranil Halder <indranilhalder.dev@gmail.com>

--- store/tests/test_collections.py
+++ store/tests/test_collections.py
@@ -11,3 +11,11 @@ class TestCreateCollection:
         response = client.post("/store/collections/", {"title": "a"})
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self):
+        client = APIClient()
+        # pyrefly: ignore [bad-argument-type]
+        client.force_authenticate(user={})
+        response = client.post("/store/collections/", {"title": "a"})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
